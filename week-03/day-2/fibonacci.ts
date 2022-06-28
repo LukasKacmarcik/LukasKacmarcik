3979704aca2d31562fe9fe7e1e8e3b553cd42788
Lukas Kacmarcik <1lukaskacmarcik1@gmail.com>
2022-06-28 16:41:35 +0200

    updating exercise from w3d2

--- week-03/day-2/fibonacci.ts
+++ week-03/day-2/fibonacci.ts
@@ -1,16 +1,25 @@
-let outcome = 0;
-let start = [0, 1];
+// let outcome = 0;
+// let start = [0, 1];
+// function fibonacciN(n: number): number {
+//   if (n < 0) {
+//     n = n * -1;
+//   }
+//   if (start.length === n) {
+//     outcome = start[start.length -1];
+//     return outcome;
+//   } else {
+//     let sum = start[start.length - 2] + start[start.length - 1];
+//     start.push(sum);
+//     return fibonacciN(n);
+//   }
+// }
+// console.log(fibonacciN(-9));
+
 function fibonacciN(n: number): number {
-  if (n < 0) {
-    n = n * -1;
-  }
-  if (start.length === n) {
-    outcome = start[start.length -1];
-    return outcome;
-  } else {
-    let sum = start[start.length - 2] + start[start.length - 1];
-    start.push(sum);
-    return fibonacciN(n);
+  if (n < 2){
+    return 1;
+  }else{
+    return fibonacciN(n-2) + fibonacciN(n-1);
   }
 }
-console.log(fibonacciN(-9));
+console.log(fibonacciN(7));
