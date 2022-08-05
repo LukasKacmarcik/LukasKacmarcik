467a757799a7cbd65c054f8ac016732060b843f9
Lukas Kacmarcik <1lukaskacmarcik1@gmail.com>
2022-08-05 14:37:10 +0200

    work in progress w7d5 redit backand

--- week-07/day-5/reddit-backend/app.js
+++ week-07/day-5/reddit-backend/app.js
@@ -1,15 +1,98 @@
+const dotenv = require('dotenv');
+const { query } = require('express');
+dotenv.config();
+
+const PORT = process.env.PORT;
+
 const express = require('express');
 const app = express();
 app.use(express.json());
 
+const connection = require('./reddit.js');
+/////GET////
 app.get('/hello', (req, res) => {
   res.status(200).send('Hello World');
 })
 
+app.get('/posts', (req, res) => {
+  const sql = 'SELECT * FROM posts;'
+  connection.query(sql, (error, results) => {
+    if (error) {
+      console.log(error);
+      return;
+    }
+    res.status(200).send(results);
+  })
+})
+////ADD////
+app.post('/posts', (req, res) => {
+  if (!req.body) {
+    res.status(400).send({ message: 'Data is missing from the body!' });
+  } else if (Object.keys(req.body).length > 3 || req.body.score) {
+    res.status(400).send({ message: 'Nice try buddy!' });
+    return;
+  }
+  const sql = `INSERT INTO posts SET ?`;
+  connection.query(sql, req.body, (error, results) => {
+    if (error) {
+      res.status(500).send({ message: 'Internal Server Error' });
+    }
+    const sql = `SELECT * FROM posts WHERE id = ?`
+    connection.query(sql, results.insertId, (error, results) => {
+      if (error) {
+        res.status(500).send({ message: 'Internal server error' })
+      }
 
-app.listen(3000, (error) => {
+      if (results.length == 0) {
+        res.status(404).send({ message: 'Post with the given id was not found' })
+      }
+
+      res.status(201).send(results);
+    })
+  })
+});
+////VOTE////
+
+app.patch('/posts/:id/upvote', (req, res) => {
+  const sql = `UPDATE posts SET score = score + 1 WHERE id = ?;`
+  connection.query(sql, req.params.id, (error, results) => {
+    if (error) {
+      console.log(error);
+      res.status(500).send({ message: 'Internal Problem with database' })
+    }
+
+    const sql = 'SELECT * FROM posts WHERE id = ?'
+    connection.query(sql, req.params.id, (error, results) => {
+      if (error) {
+        res.status(500).send({ message: 'Internal problem with database2' })
+      }
+      res.status(200).send(results);
+    })
+  })
+})
+
+app.patch('/posts/:id/downvote', (req, res) => {
+  const sql = `UPDATE posts SET score = score - 1 WHERE id = ?;`
+  connection.query(sql, req.params.id, (error, results) => {
+    if (error) {
+      console.log(error);
+      res.status(500).send({ message: 'Internal Problem with database' })
+    }
+
+    const sql = 'SELECT * FROM posts WHERE id = ?'
+    connection.query(sql, req.params.id, (error, results) => {
+      if (error) {
+        res.status(500).send({ message: 'Internal problem with database2' })
+      }
+      res.status(200).send(results);
+    })
+  })
+})
+
+app.listen(PORT, (error) => {
   if (error) {
     console.log(error);
+    return;
   }
   console.log('Reddit is lisening at 3000');
 });
