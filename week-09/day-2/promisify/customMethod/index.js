0ecfccf272fa55b1fef68f65cf2f6fc27afc8acf
Lukas Kacmarcik <1lukaskacmarcik1@gmail.com>
2022-08-16 17:05:11 +0200

    promisify query finished w9d2

---
+++ week-09/day-2/promisify/customMethod/index.js
@@ -0,0 +1,14 @@
+function loadScript(src, callback) {
+  let script = document.createElement('script');
+  script.src = src;
+
+  script.onload = () => callback(null, script);
+  script.onerror = () => callback(new Error(`Script load error for ${src}`));
+
+  document.head.append(script);
+}
+
+// usage:
+// loadScript('path/script.js', (err, script) => {...})
+
+loadScript()
