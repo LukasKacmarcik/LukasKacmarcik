91c7fbb95ae69c480d12260abe96c641968ff4c5
Lukas Kacmarcik <1lukaskacmarcik1@gmail.com>
2022-08-19 13:01:49 +0200

    quizz app almost finished

--- week-09/day-4/quizapp/public/manage.js
+++ week-09/day-4/quizapp/public/manage.js
@@ -1,20 +1,86 @@
 const layout = document.querySelector('#layout');
 const questions = document.querySelector('#questions');
+const form = document.forms[0];
+const question = document.querySelector('#inputQuestion');
+const answer1 = document.querySelector('#answer1');
+const answer2 = document.querySelector('#answer2');
+const answer3 = document.querySelector('#answer3');
+const answer4 = document.querySelector('#answer4');
+const button = document.querySelector('button');
 
-fetch(`/api/questions`)
-  .then(res => res.json())
-  .then(data => {
-    data.forEach(question => {
-      const newQuestion = document.createElement('div');
-      newQuestion.className = 'question'
-      const newQuestionContent = document.createElement('p');
-      newQuestionContent.className = 'questionContent';
-      newQuestion.textContent = question.question;
-      const newDelete = document.createElement('p');
-      newDelete.textContent = 'delete';
-      newDelete.className = 'delete';
-      newQuestion.appendChild(newQuestionContent);
-      newQuestion.appendChild(newDelete);
-      questions.appendChild(newQuestion);
-    });
-  })
+function fetchQuestions() {
+  questions.innerHTML = "";
+  fetch(`/api/questions`)
+    .then(res => res.json())
+    .then(data => {
+      data.forEach(question => {
+        const newQuestion = document.createElement('div');
+        newQuestion.className = 'question';
+        newQuestion.setAttribute('data-id', question.id);
+        const newQuestionContent = document.createElement('p');
+        newQuestionContent.className = 'questionContent';
+        newQuestion.textContent = question.question;
+        const newDelete = document.createElement('p');
+        newDelete.textContent = 'delete';
+        newDelete.className = 'delete';
+        newQuestion.appendChild(newQuestionContent);
+        newQuestion.appendChild(newDelete);
+        questions.appendChild(newQuestion);
+      });
+    })
+};
+fetchQuestions();
+
+questions.addEventListener('click', (e) => {
+  if (e.target.className == 'delete') {
+    deleteQuestion(e);
+  }
+});
+
+button.addEventListener('click', (e) => {
+  if (question.value && answer1.value && answer2.value && answer3.value && answer4.value && (answer1.nextElementSibling.checked || answer2.nextElementSibling.checked || answer3.nextElementSibling.checked || answer4.nextElementSibling.checked)) {
+    const newQuestion = {
+      question: question.value,
+      answers: [
+        {
+          answer_1: answer1.value,
+          is_correct: (answer1.nextElementSibling.checked ? 1 : 0)
+        },
+        {
+          answer_2: `${answer2.value}`,
+          is_correct: (answer2.nextElementSibling.checked ? 1 : 0)
+        },
+        {
+          answer_3: `${answer3.value}`,
+          is_correct: (answer3.nextElementSibling.checked ? 1 : 0)
+        },
+        {
+          answer_4: `${answer4.value}`,
+          is_correct: (answer4.nextElementSibling.checked ? 1 : 0)
+        }
+      ]
+    };
+    fetch(`/api/questions`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(newQuestion)
+    })
+      .then(res => {
+        form.reset();
+        fetchQuestions();
+      })
+  }
+})
+
+
+
+
+
+function deleteQuestion(e) {
+  fetch(`/api/questions/${e.target.parentElement.dataset.id}`, {
+    method: 'DELETE'
+  });
+  e.target.parentElement.style.display = 'none';
+};
