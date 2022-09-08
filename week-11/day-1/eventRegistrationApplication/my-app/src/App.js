29b4532d0f4743dc46b1b9503ffc3be2a8260413
Lukas Kacmarcik <1lukaskacmarcik1@gmail.com>
2022-09-08 13:19:38 +0200

    project done -> eventRegistrationApplication

--- week-11/day-1/eventRegistrationApplication/my-app/src/App.js
+++ week-11/day-1/eventRegistrationApplication/my-app/src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const [form, setForm] = useState({
+    firstName: "",
+    secondName: "",
+    password: ""
+  })
+
+  const [submitted, setSubmitted] = useState(false);
+
+  function updateForm(e) {
+    setSubmitted(false);
+    setForm(ps => {
+      return {
+        ...ps,
+        [e.target.name]: e.target.value
+      }
+    })
+  }
+
+  function submit(e) {
+    e.preventDefault();
+    setSubmitted(true);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={submit}>
+        <label>
+          <input type="text" id="firstName" name="firstName" placeholder="firstName" value={form.firstName} onChange={updateForm} />
+        </label>
+        <label>
+          <input type="text" id="secondName" name="secondName" placeholder="secondName" value={form.secondName} onChange={updateForm} />
+        </label>
+        <label>
+          <input type="password" id="password" name="password" placeholder="password" value={form.password} onChange={updateForm} />
+        </label>
+        <button>Submit!</button>
+      </form>
+      {submitted &&
+        <ul>
+          {Object.entries(form).map(([key, value]) => <li key={key}>{key}: {value}</li>)}
+        </ul>}
     </div>
   );
 }
