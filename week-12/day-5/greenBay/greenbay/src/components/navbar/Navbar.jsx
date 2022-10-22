3f2d3aa9e6841e8fb663249cea668540c3051fbc
Lukas Kacmarcik <1lukaskacmarcik1@gmail.com>
2022-10-22 11:29:35 +0200

    style navbar and add registration link

--- week-12/day-5/greenBay/greenbay/src/components/navbar/Navbar.jsx
+++ week-12/day-5/greenBay/greenbay/src/components/navbar/Navbar.jsx
@@ -1,3 +1,28 @@
-function Navbar() {}
+import { Link } from "react-router-dom";
+import {
+  Navbar as NavbarReactstrap,
+  NavbarBrand,
+  NavItem,
+  NavLink,
+  Nav,
+} from "reactstrap";
+import styles from "./navbar.module.scss";
+
+function Navbar() {
+  return (
+    <NavbarReactstrap className={styles.navbar}>
+      <NavbarBrand to="/" tag={Link}>
+        <img src="brand.png" alt="brand" />
+      </NavbarBrand>
+      <Nav>
+        <NavItem className={styles.navitem}>
+          <NavLink to="/signup" tag={Link}>
+            <i className="fas fa-user-alt" style={{ fontSize: "36px" }} />
+          </NavLink>
+        </NavItem>
+      </Nav>
+    </NavbarReactstrap>
+  );
+}
 
 export default Navbar;
