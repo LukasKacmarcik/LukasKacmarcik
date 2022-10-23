30909c27139b3f094ee38ada2f953632cdbce235
Lukas Kacmarcik <1lukaskacmarcik1@gmail.com>
2022-10-23 18:23:34 +0200

    delete useless code

--- week-12/day-5/greenBay/greenbay/server/controllers/items.js
+++ week-12/day-5/greenBay/greenbay/server/controllers/items.js
@@ -27,9 +27,8 @@ const buyItem = async (req, res) => {
       },
     });
     if (!item) {
-      console.log(item, "ite 30");
       isValid = false;
-      message.buyItemErrors += "Item is not fount. ";
+      message.buyItemErrors += "Item is not found. ";
     } else {
       if (item.buyer_id) {
         isValid = false;
@@ -157,151 +156,9 @@ const addItem = async (req, res) => {
   }
 };
 
-// update a restaurant by id
-const updateRestaurant = async (req, res) => {
-  // try {
-  //   await uploadRestaurant(req, res);
-  // } catch (err) {
-  //   res.status(400).json({ message: err.message });
-  //   return;
-  // }
-
-  let isValid = true;
-  let message = "";
-
-  if (!req.body.name.trim()) {
-    message += "The Restaurant name field is required. ";
-    isValid = false;
-  }
-
-  if (!req.body.address.trim()) {
-    message += "The address field is required. ";
-    isValid = false;
-  }
-
-  if (!req.body.email.trim()) {
-    message += "The email field is required. ";
-    isValid = false;
-  }
-
-  if (!req.body.phone_number.trim()) {
-    message += "The phone number field is required. ";
-    isValid = false;
-  }
-
-  if (!req.body.delivery_fee.trim()) {
-    message += "The delivery fee field is required. ";
-    isValid = false;
-  }
-
-  if (isNaN(req.body.delivery_fee.trim())) {
-    message += "The delivery fee must be number. ";
-    isValid = false;
-  }
-
-  if (!isValid) {
-    res.status(400).json({ message });
-    return;
-  }
-  try {
-    const restaurant = await prisma.restaurant.findUnique({
-      where: {
-        id: req.params.id,
-      },
-    });
-    if (restaurant) {
-      // Have to use updateMany bc update does not support multiple where conditions.
-      const updatedRestaurantData = await prisma.restaurant.updateMany({
-        where: {
-          id: req.params.id,
-          owner_id: parseInt(req.userId),
-        },
-        data: {
-          name: req.body.name,
-          description: req.body.description,
-          address: req.body.address,
-          email: req.body.email,
-          phone_number: req.body.phone_number,
-          delivery_fee: parseFloat(req.body.delivery_fee),
-          img_url:
-            Math.floor(Date.now() / 10000) +
-            req.file.originalname.replace(/\s/g, "_"),
-          owner_id: req.userId,
-        },
-      });
-      res.send(updatedRestaurantData);
-    } else {
-      res.status(404).json({ message: "Restaurant not found" });
-    }
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: err.message });
-  }
-};
-
-// get a restaurant by id
-const getRestaurantById = async (req, res) => {
-  try {
-    const restaurant = await prisma.restaurant.findUnique({
-      where: {
-        id: req.params.id,
-      },
-    });
-    if (restaurant) {
-      res.send(restaurant);
-    } else {
-      res.status(404).json({ message: "Restaurant not found" });
-    }
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: err.message });
-  }
-};
-
-const deleteRestaurantById = async (req, res) => {
-  try {
-    const existenceOfForeignKeys = await prisma.meals.findFirst({
-      where: {
-        restaurant_id: req.params.id,
-      },
-    });
-    if (existenceOfForeignKeys) {
-      res
-        .status(400)
-        .json({ message: "Restaurant have meals and cannot be removed" });
-      return;
-    }
-    const verifyExistence = await prisma.restaurant.findUnique({
-      where: {
-        id: req.params.id,
-      },
-    });
-    if (!verifyExistence) {
-      res.status(404).json({ message: "Restaurant not Found" });
-      return;
-    }
-
-    // Have to use deleteMany bc delete does not support multiple where conditions.
-    const removedRestaurant = await prisma.restaurant.deleteMany({
-      where: {
-        id: req.params.id,
-        owner_id: parseInt(req.userId),
-      },
-    });
-    if (removedRestaurant.count === 1)
-      res.status(200).json({ message: "Restaurant deleted" });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: err.message });
-  }
-};
-
 // export the functions
 module.exports = {
   getItems,
   addItem,
   buyItem,
-  updateRestaurant,
-  getRestaurantById,
-  deleteRestaurantById,
 };
